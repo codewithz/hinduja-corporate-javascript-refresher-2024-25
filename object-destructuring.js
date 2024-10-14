e3d976013f9d245f299fc0b41937a652123cf9d4
Zartab Nakhwa <zartab@codewithz.com>
2024-10-14 10:45:42 +0530

    Object Destructuring - Part 2

--- object-destructuring.js
+++ object-destructuring.js
@@ -20,3 +20,29 @@ console.log("City:",city)
 console.log("Pincode:",pincode)
 console.log("State:",state)
 console.log("St:",st)
+
+
+console.log("-------------- DESCTRUCTURING AND FUNCTIONS--------------------")
+
+function displayAddress(address){
+    console.log("-----------1----------------")
+    console.log(address.city)
+    console.log(address.state)
+    console.log(address.pincode)
+    console.log(address.country)
+
+}
+
+displayAddress(address)
+
+// const {city,pincode,state,country}=address
+
+function displayAddressNewWay({city,pincode,state,country}){
+    console.log("-----------2----------------")
+    console.log(city)
+    console.log(state)
+    console.log(pincode)
+    console.log(country)
+}
+
+displayAddressNewWay(address)
